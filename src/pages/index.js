ccf125f8f47f0bd2f509fa977b9ae9d0d4806aa1
Ryan Wiemer <ryan@ryanwiemer.com>
2018-01-08 14:09:04 -0800

    Starting to add content to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,22 @@ const IndexPage = () => {
         <meta property="og:image:height" content="1200" />
       </Helmet>
 
+      <section className="bio">
+
+        <h2>Hello there!</h2>
+        <p>
+          My name is Ryan Wiemer and I am an account manager that enjoys working on web or interactive projects. While client calls and scrum meetings take up most of my day I strive to keep my development skills sharp with open source and other side projects.
+        </p>
+
+        <button>learn more</button>
+
+        <p>
+          I've been lucky enough to manage projects for some cool folks such as Cooley, Pacific Union, ProSight and many, many more. Unfortuanatley due to ongoing development or other legal obligations I can't show it here. 
+        </p>
+
+
+      </section>
+
 
     </div>
   )
