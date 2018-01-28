a39a29165ff15b312bf179dbd6d1708e9858b390
Ryan Wiemer <ryan@ryanwiemer.com>
2018-01-27 19:22:30 -0800

    Scrolling improvements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,15 @@
-// Always start at the top of the page on a route change.
-// More investigation needed for a better solution. See https://github.com/gatsbyjs/gatsby/pull/3483
+// Scrolling improvements as suggested by @dsbrianwebster
+// See https://github.com/ryanwiemer/rw/issues/1
 
-exports.onRouteUpdate = () => {
-  if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
+exports.onRouteUpdate = args => {
+  if (typeof window !== 'undefined' && args.action == "PUSH") {
+    window.scrollTo(0, 0)
+  }
 }
 
 exports.shouldUpdateScroll = args => {
-   return false;
+  return (
+    args.prevRouterProps == null ||
+    (args.prevRouterProps && args.prevRouterProps.history.action == "POP")
+  ) ? true : false;
 };
