55fde1418824e1ec43bb1e9e9b76e9be63bfe4fa
Ryan Wiemer <ryan@ryanwiemer.com>
2019-03-06 15:40:27 -0800

    SEO Fix

--- src/components/general/SEO.js
+++ src/components/general/SEO.js
@@ -29,34 +29,34 @@ const SEO = props => (
     {typeof window !== `undefined` && (
       <meta property="og:url" content={location.href} />
     )}
-    <meta
-      property="og:image"
-      content={props.image ? props.image.ogimg.src : defaultImage}
-    />
-
     {props.image ? (
-      <meta property="og:image:width" content={props.image.ogimg.width} />
+      <>
+        <meta
+          property="og:image"
+          content={
+            props.image ? `https:${props.image.ogimg.src}` : defaultImage
+          }
+        />
+        <meta property="og:image:width" content={props.image.ogimg.width} />
+        <meta property="og:image:height" content={props.image.ogimg.height} />
+      </>
     ) : null}
-    {props.image ? (
-      <meta property="og:image:height" content={props.image.ogimg.height} />
-    ) : null}
-
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content="@ryanwiemer" />
     <meta
       name="twitter:title"
       content={props.title ? `${props.title} - ${defaultTitle}` : defaultTitle}
     />
-    {props.image ? (
-      <meta
-        name="twitter:image"
-        content={props.image ? props.image.ogimg.src : defaultImage}
-      />
-    ) : null}
     <meta
       name="twitter:description"
       content={props.description ? props.description : defaultDescription}
     />
+    {props.image ? (
+      <meta
+        name="twitter:image"
+        content={props.image ? `https:${props.image.ogimg.src}` : defaultImage}
+      />
+    ) : null}
   </Helmet>
 )
 
