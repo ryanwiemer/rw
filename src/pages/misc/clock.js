cf700efa950ba86e41a751d3d6bc6593a1616c75
ryanwiemer <ryan@ryanwiemer.com>
2020-01-12 23:25:17 -0800

    Playing around with the clock.

--- src/pages/misc/clock.js
+++ src/pages/misc/clock.js
@@ -26,11 +26,28 @@ const Time = styled.h2`
   }
 `
 
+const Controls = styled.div`
+  padding: 0;
+  position: fixed;
+  bottom: 1.5em;
+  left: 1.5em;
+  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
+    left: 2.5em;
+  }
+`
+
+const Label = styled.label`
+  cursor: pointer;
+  user-select: none;
+  opacity: ${props => (props.active ? '1' : '.25')};
+`
+
 const Circle = styled(motion.div)`
   background: linear-gradient(
-    to bottom right,
-    ${props => props.theme.colors.accent},
-    #8b4e04
+    -60deg,
+    hsla(218, 50%, 31%, 1) 0%,
+    hsla(277, 51%, 12%, 1) 76%,
+    hsla(277, 41%, 9%, 1) 100%
   );
   z-index: -2;
   width: 300px;
@@ -49,7 +66,7 @@ const Circle = styled(motion.div)`
   }
 `
 
-const Line = styled(motion.div)`
+const Hand = styled(motion.div)`
   position: absolute;
   z-index: 0;
   width: 2px;
@@ -65,7 +82,6 @@ const Line = styled(motion.div)`
     top: 0;
     left: 0;
     width: 100%;
-    height: 50%;
     background: white;
     opacity: 0.5;
   }
@@ -83,8 +99,51 @@ const Line = styled(motion.div)`
   }
 `
 
+const SecondHand = styled(Hand)`
+  &::before {
+    height: 50%;
+  }
+`
+
+const MinuteHand = styled(Hand)`
+  &::before {
+    height: 40%;
+    top: 10%;
+  }
+`
+
+const HourHand = styled(Hand)`
+  &::before {
+    height: 30%;
+    top: 20%;
+  }
+`
+
 const Clock = () => {
   const [date, setDate] = useState(new Date())
+  const [seconds, setSeconds] = useState(false)
+  const [minutes, setMinutes] = useState(true)
+  const [hours, setHours] = useState(true)
+  const [digits, setDigits] = useState(false)
+  const [full, setFull] = useState(false)
+
+  function toggleSeconds() {
+    setSeconds(!seconds)
+  }
+  function toggleMinutes() {
+    setMinutes(!minutes)
+  }
+  function toggleHours() {
+    setHours(!hours)
+  }
+  function toggleDigits() {
+    setDigits(!digits)
+  }
+  function toggleFull() {
+    setFull(!full)
+    // Add fullscreen logic here
+  }
+
   useEffect(() => {
     let timerID = setInterval(() => tick(), 1000)
     return function cleanup() {
@@ -102,20 +161,75 @@ const Clock = () => {
     hour12: true,
   })
 
-  const seconds = date.toLocaleTimeString([], {
+  const currentSeconds = date.toLocaleTimeString([], {
     second: '2-digit',
   })
 
-  const rotate = seconds * 6
+  const currentMinutes = date.toLocaleTimeString([], {
+    minute: '2-digit',
+  })
+
+  const currenHours = date.toLocaleTimeString([], {
+    hour: 'numeric',
+    hour12: false,
+  })
+
+  const rotateSeconds = currentSeconds * 6
+  const rotateMinutes = currentMinutes * 6
+  const rotateHours = currenHours * 15
 
   return (
     <>
       <SEO title="Clock" description="A very simple clock made for fun" />
       <Wrapper>
         <Circle>
-          <Line style={{ rotate: rotate }} />
-          <Time>{time}</Time>
+          {hours && <HourHand style={{ rotate: rotateHours }} />}
+          {minutes && <MinuteHand style={{ rotate: rotateMinutes }} />}
+          {seconds && <SecondHand style={{ rotate: rotateSeconds }} />}
+          {digits && <Time>{time}</Time>}
         </Circle>
+        <Controls>
+          <input
+            type="checkbox"
+            id="digits"
+            name="digits"
+            onClick={toggleDigits}
+          />
+          <Label htmlFor="digits" active={digits}>
+            Digits
+          </Label>
+          <input
+            type="checkbox"
+            id="hours"
+            name="hours"
+            onClick={toggleHours}
+          />
+          <Label htmlFor="hours" active={hours}>
+            Hours
+          </Label>
+          <input
+            type="checkbox"
+            id="minutes"
+            name="minutes"
+            onClick={toggleMinutes}
+          />
+          <Label htmlFor="minutes" active={minutes}>
+            Minutes
+          </Label>
+          <input
+            type="checkbox"
+            id="seconds"
+            name="seconds"
+            onClick={toggleSeconds}
+          />
+          <Label htmlFor="seconds" active={seconds}>
+            Seconds
+          </Label>
+          <input type="checkbox" id="full" name="full" onClick={toggleFull} />
+          <Label htmlFor="full" active={full}>
+            Fullscreen
+          </Label>
+        </Controls>
       </Wrapper>
     </>
   )
