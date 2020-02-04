6114a765bde72728e45b976426dab76bddc003e4
ryanwiemer <ryan@ryanwiemer.com>
2020-02-04 13:04:40 -0800

    Organize files and SEO tweaks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,24 +1,4 @@
-import React from 'react'
-import Layout from './src/components/general/Layout'
-
-const transitionDelay = 350
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
-
-export const shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition,
-}) => {
-  if (location.action === 'PUSH') {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
-  } else {
-    const savedPosition = getSavedScrollPosition(location)
-    window.setTimeout(
-      () => window.scrollTo(...(savedPosition || [0, 0])),
-      transitionDelay
-    )
-  }
-  return false
-}
+import scrollDelay from './src/gatsby/browser/shouldUpdateScroll'
+import CustomLayout from './src/gatsby/browser/wrapPageElement'
+export const wrapPageElement = CustomLayout
+export const shouldUpdateScroll = scrollDelay
