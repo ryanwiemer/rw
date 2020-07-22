9d4b001055a14bf0ba7982932f331c8f9d8cfacc
ryanwiemer <ryan@ryanwiemer.com>
2020-07-21 22:27:33 -0700

    Add message to Work page

--- src/components/templates/WorkList.js
+++ src/components/templates/WorkList.js
@@ -5,14 +5,14 @@ import Img from 'gatsby-image'
 
 const Wrapper = styled.div`
   padding: 2.5em 1.5em;
-  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
     padding: 2.5em 3em;
   }
 `
 
 const Header = styled.div`
   margin: 0 0 2em 0;
-  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
     margin: 0 0 4em 0;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
@@ -23,21 +23,48 @@ const Header = styled.div`
 const Title = styled.h1`
   line-height: 1.15;
   letter-spacing: -0.01em;
-  font-weight: ${props => props.theme.fontWeights.bold};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
   font-size: 1.866em;
-  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
     font-size: 2.488em;
+    grid-column: span 1;
+  }
+`
+
+const Aside = styled.p`
+  font-size: 0.9em;
+  background: ${(props) => props.theme.colors.muted};
+  padding: 1em;
+  border-radius: 3px;
+  font-size: 1em;
+  margin: 1rem 0 0 0;
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
+    margin: 0;
+    font-size: 1em;
     grid-column: span 2;
+  }
+  em {
+    font-weight: ${(props) => props.theme.fontWeights.bold};
+  }
+  a {
+    transition: 0.3s color;
+    color: ${(props) => props.theme.colors.text};
+    &:hover {
+      color: ${(props) => props.theme.colors.accent};
+    }
+    @media (hover: none) {
+      color: ${(props) => props.theme.colors.text} !important;
+    }
   }
 `
 
 const List = styled.ul`
-  @media screen and (min-width: ${props => props.theme.responsive.small}) {
+  @media screen and (min-width: ${(props) => props.theme.responsive.small}) {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-column-gap: 0.75em;
   }
-  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-column-gap: 1.5em;
@@ -52,9 +79,9 @@ const Heading = styled.h2`
   transition: 0.3s color;
   margin: 1rem 0 0 0;
   font-size: 1.296em;
-  font-weight: ${props => props.theme.fontWeights.semiBold};
+  font-weight: ${(props) => props.theme.fontWeights.semiBold};
   line-height: 1.15;
-  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
     font-size: 1.44em;
   }
 `
@@ -73,10 +100,10 @@ const Category = styled.span`
 
 const ProjectLink = styled(Link)`
   text-decoration: none;
-  color: ${props => props.theme.colors.text};
+  color: ${(props) => props.theme.colors.text};
   &:hover {
     h2 {
-      color: ${props => props.theme.colors.accent};
+      color: ${(props) => props.theme.colors.accent};
     }
     p {
       opacity: 0.5;
@@ -84,7 +111,7 @@ const ProjectLink = styled(Link)`
   }
   @media (hover: none) {
     h2 {
-      color: ${props => props.theme.colors.text} !important;
+      color: ${(props) => props.theme.colors.text} !important;
     }
     p {
       opacity: 1 !important;
@@ -92,11 +119,16 @@ const ProjectLink = styled(Link)`
   }
 `
 
-const WorkList = props => {
+const WorkList = (props) => {
   return (
     <Wrapper>
       <Header>
         <Title>Selected Work</Title>
+        <Aside>
+          <em>Note:</em> This is only a small sample of client work and personal
+          projects. To learn more please get in{' '}
+          <Link to="/contact/">contact</Link>. 😀
+        </Aside>
       </Header>
       <List>
         {props.projects.map(({ node: project }) => (
