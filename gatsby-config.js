dc0c266de3cd7945968ae2738fc096a52e7015b9
Ryan Wiemer <ryan@ryanwiemer.com>
2021-07-31 18:59:26 -0700

    Env (#18)
    * Use environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,15 @@
-let contentfulConfig
-
-try {
-  contentfulConfig = require('./.contentful')
-} catch (e) {
-  contentfulConfig = {
-    production: {
-      spaceId: process.env.SPACE_ID,
-      accessToken: process.env.ACCESS_TOKEN,
-    },
-  }
-} finally {
-  const { spaceId, accessToken } = contentfulConfig.production
-  if (!spaceId || !accessToken) {
-    throw new Error('Contentful space ID and access token need to be provided.')
-  }
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+}
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
 }
 
 module.exports = {
@@ -70,10 +66,7 @@ module.exports = {
     },
     {
       resolve: 'gatsby-source-contentful',
-      options:
-        process.env.NODE_ENV === 'development'
-          ? contentfulConfig.development
-          : contentfulConfig.production,
+      options: contentfulConfig,
     },
     'gatsby-plugin-netlify',
   ],
