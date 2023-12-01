1a639c90f45f3bcd69a412b0812840c1ad05ca98
Ryan Wiemer <ryan@ryanwiemer.com>
2023-11-30 23:31:33 -0800

    Stage (#29)
    * Custom 404
    * Update sitemap and robots.txt settings

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <div className="text-center container flex justify-center items-center min-h-[100svh] ">
+      <div className="my-24 flex align-baseline items-baseline">
+        <div>
+          <h1 className="text-4xl md:text-6xl font-extrabold tracking-tight">
+            404
+          </h1>
+          <p className="mt-4">Page not found.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const stagePolicies = [
+  {
+    userAgent: '*',
+    disallow: '/',
+  },
+]
+
+const prodPolicies = [
+  {
+    userAgent: '*',
+    allow: '/',
+  },
+]
+
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.ryanwiemer.com',
-  generateRobotsTxt: process.env.PREVIEW_ENV ? false : true,
+  generateIndexSitemap: false,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: process.env.STAGE === 'true' ? stagePolicies : prodPolicies,
+  },
 }
